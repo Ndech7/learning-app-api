1d811f7227baf893cf3fe3879ef269a568580b92
Ndech7 <stephennderitu3@gmail.com>
2023-10-05 11:58:44 +0300

    Update allowed origins

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "https://learning-app-ocs8.onrender.com/",
+  origin: "https://learning-app-ocs8.onrender.com",
 };
 
 // allow CORS
